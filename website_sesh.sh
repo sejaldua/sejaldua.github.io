1a5a9295dad230145a5a8b7dc24b17bcb61eb674
Sejal <sejal.dua@tufts.edu>
2019-12-10 21:37:56 +0100

    made a bash script to open up VSCode files all at once-- boost productivity

---
+++ website_sesh.sh
@@ -0,0 +1,6 @@
+open -a "Visual Studio Code.app" /Users/sejaldua/Desktop/sejaldua.github.io/index.html
+open -a "Visual Studio Code.app" /Users/sejaldua/Desktop/sejaldua.github.io/projects.html
+open -a "Visual Studio Code.app" /Users/sejaldua/Desktop/sejaldua.github.io/articles.html
+open -a "Visual Studio Code.app" /Users/sejaldua/Desktop/sejaldua.github.io/timeline.html
+open -a "Visual Studio Code.app" /Users/sejaldua/Desktop/sejaldua.github.io/music.html
+open -a "Visual Studio Code.app" /Users/sejaldua/Desktop/sejaldua.github.io/links.html
