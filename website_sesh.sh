c57372b2f2e51b2015d0a11ca239d4dbeb7931ee
sejaldua <sejal.dua@tufts.edu>
2021-04-12 16:36:27 -0400

    fixed bug! thanks, peter

--- website_sesh.sh
+++ website_sesh.sh
@@ -1,6 +1,6 @@
-open -a "Visual Studio Code.app" /Users/sejaldua/Desktop/sejaldua.github.io/index.html
-open -a "Visual Studio Code.app" /Users/sejaldua/Desktop/sejaldua.github.io/projects.html
-open -a "Visual Studio Code.app" /Users/sejaldua/Desktop/sejaldua.github.io/articles.html
-open -a "Visual Studio Code.app" /Users/sejaldua/Desktop/sejaldua.github.io/timeline.html
-open -a "Visual Studio Code.app" /Users/sejaldua/Desktop/sejaldua.github.io/music.html
-open -a "Visual Studio Code.app" /Users/sejaldua/Desktop/sejaldua.github.io/links.html
+open -a "Visual Studio Code.app" /Users/sejaldua/Desktop/DESKTOP/sejaldua.github.io/index.html
+open -a "Visual Studio Code.app" /Users/sejaldua/Desktop/DESKTOP/sejaldua.github.io/projects.html
+open -a "Visual Studio Code.app" /Users/sejaldua/Desktop/DESKTOP/sejaldua.github.io/articles.html
+open -a "Visual Studio Code.app" /Users/sejaldua/Desktop/DESKTOP/sejaldua.github.io/timeline.html
+open -a "Visual Studio Code.app" /Users/sejaldua/Desktop/DESKTOP/sejaldua.github.io/music.html
+open -a "Visual Studio Code.app" /Users/sejaldua/Desktop/DESKTOP/sejaldua.github.io/links.html
